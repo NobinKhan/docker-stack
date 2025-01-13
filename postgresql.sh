b6eee577c8b44769b1e781efce4c446230aa8bd3
nobinkhan <nazrulkhan.dk@gmail.com>
2025-01-13 14:49:56 +0600

    fix: postgres fixed

--- postgresql.sh
+++ postgresql.sh
@@ -14,7 +14,7 @@ fi
 # Build docker image (corrected Dockerfile path and quoting)
 RUNNER=$(command -v podman || command -v docker)  # Use whichever is available
 
-CONTAINER_IMAGE="nobinkhan/postgresql:latest"
+CONTAINER_IMAGE="cgr.dev/chainguard/postgres:latest"
 CONTAINER_NAME="postgresql"
 CONTAINER_PORT="5432"
 HOST_PORT="5432"
@@ -66,8 +66,7 @@ CONTAINER_ID=$($RUNNER run \
   --publish $HOST_PORT:$CONTAINER_PORT \
   --env POSTGRES_USER=${DATABASE_USER} \
   --env POSTGRES_PASSWORD=${DATABASE_PASSWORD} \
-  --volume postgresql_data1:/data/postgres \
-  --volume postgresql_data2:/var/lib/postgresql \
+  --volume postgresql_data2:/var/lib/postgresql/data \
   $CONTAINER_IMAGE)
 
 # Check if the container started successfully
